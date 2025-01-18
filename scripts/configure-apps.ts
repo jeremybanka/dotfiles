3b1c19f93f191a8137e924e493c7ed3f753703fa
Jeremy Banka <hello@jeremybanka.com>
2025-01-17 21:27:50 -0800

    🔧 configure indesign workspace (#196)

--- scripts/configure-apps.ts
+++ scripts/configure-apps.ts
@@ -10,6 +10,7 @@ const appConfigDir = join(import.meta.dirname, `..`, `apps`)
 await configureBun()
 await configureVSCodium()
 await configureIllustrator()
+await configureInDesign()
 
 async function configureBun() {
 	const sourceDir = resolve(appConfigDir, `bun`)
@@ -124,6 +125,96 @@ async function configureIllustrator() {
 			`Workspaces`
 		)
 		console.log(`Configuring Illustrator`, {
+			appConfigDir,
+			sourceWorkspacesDir,
+			targetWorkspacesDir,
+		})
+
+		if (!existsSync(targetWorkspacesDir)) {
+			mkdirSync(targetWorkspacesDir, { recursive: true })
+		}
+
+		const sourceWorkspaces = readdirSync(sourceWorkspacesDir)
+		for (const workspace of sourceWorkspaces) {
+			const sourceWorkspacePath = join(sourceWorkspacesDir, workspace)
+			const targetWorkspacePath = join(targetWorkspacesDir, workspace)
+
+			if (existsSync(targetWorkspacePath)) {
+				const isSymlink = lstatSync(targetWorkspacePath).isSymbolicLink()
+				if (!isSymlink) {
+					console.log(`Removing existing file: ${targetWorkspacePath}`)
+					await unlink(targetWorkspacePath)
+				}
+			}
+
+			await symlink(sourceWorkspacePath, targetWorkspacePath)
+			console.log(
+				`Symlink created: ${sourceWorkspacePath} -> ${targetWorkspacePath}`
+			)
+		}
+
+		const targetWorkspaces = readdirSync(targetWorkspacesDir)
+		for (const workspace of targetWorkspaces) {
+			const sourceWorkspacePath = join(sourceWorkspacesDir, workspace)
+			const targetWorkspacePath = join(targetWorkspacesDir, workspace)
+
+			if (!existsSync(sourceWorkspacePath)) {
+				const newSourceWorkspacePath = join(sourceWorkspacesDir, workspace)
+				console.log(
+					`Moving existing workspace to source: ${targetWorkspacePath} -> ${newSourceWorkspacePath}`
+				)
+				await rename(targetWorkspacePath, newSourceWorkspacePath)
+				await symlink(newSourceWorkspacePath, targetWorkspacePath)
+				console.log(
+					`Symlink created: ${newSourceWorkspacePath} -> ${targetWorkspacePath}`
+				)
+			}
+		}
+	} catch (thrown) {
+		if (thrown instanceof Error) {
+			console.error(thrown.message)
+		} else {
+			console.error(thrown)
+		}
+	}
+}
+
+async function configureInDesign() {
+	const sourceWorkspacesDir = resolve(appConfigDir, `InDesign`, `Workspaces`)
+	const inDesignPrefsDir = join(
+		homedir(),
+		`Library`,
+		`Preferences`,
+		`Adobe InDesign`
+	)
+	const versionPattern = /^Version (\d+).0$/
+
+	try {
+		if (!existsSync(sourceWorkspacesDir)) {
+			throw new Error(
+				`Source workspaces directory does not exist: ${sourceWorkspacesDir}`
+			)
+		}
+
+		const dirs = readdirSync(inDesignPrefsDir)
+		const inDesignDirs = dirs.filter((dir) => versionPattern.test(dir))
+		if (inDesignDirs.length === 0) {
+			throw new Error(`No Adobe InDesign settings directories found.`)
+		}
+
+		const highestVersionDir = inDesignDirs.sort((a, b) => {
+			const aVersion = parseInt(a.match(versionPattern)![1], 10)
+			const bVersion = parseInt(b.match(versionPattern)![1], 10)
+			return bVersion - aVersion
+		})[0]
+
+		const targetWorkspacesDir = join(
+			inDesignPrefsDir,
+			highestVersionDir,
+			`en_US`,
+			`Workspaces`
+		)
+		console.log(`Configuring InDesign`, {
 			appConfigDir,
 			sourceWorkspacesDir,
 			targetWorkspacesDir,
