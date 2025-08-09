2b21fa6c5f9f84f94072f10398560fffdc09ce91
Jeremy Banka <hello@jeremybanka.com>
2025-08-08 17:26:33 -0700

    ❓ add some more visibility into setup

--- setup.sh
+++ setup.sh
@@ -16,7 +16,11 @@ for item in "$VIRTUAL_HOME"/* "$VIRTUAL_HOME"/.[^.]*; do
 	fi
 done
 
+ls -l "$HOME/.bun/install"
 rm -rf "$HOME/.bun/install/global"
+echo "Removing old global bun folder"
 ln -sf "$VIRTUAL_HOME"/.bun/install/global "$HOME/.bun/install/global"
+echo "Symlinked new global bun folder"
+ls -l "$HOME/.bun/install"
 
 echo "All dotfiles and configurations have been symlinked."
