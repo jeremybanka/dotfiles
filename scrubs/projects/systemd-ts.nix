574309dfbf508c6ce2e9b641c881d17365f9fc93
Jeremy Banka <hello@jeremybanka.com>
2026-05-16 20:00:37 -0700

    Fix systemd-ts sandbox permissions in scrubs

--- scrubs/projects/systemd-ts.nix
+++ scrubs/projects/systemd-ts.nix
@@ -1,7 +1,34 @@
 { lib, pkgs, ... }:
+let
+  dockerStateRoot = "/home/jem/systemd-ts/.docker";
+in
 {
   environment.systemPackages = with pkgs; [
+    acl
     docker-buildx
+  ];
+
+  environment.sessionVariables = {
+    SYSTEMD_TS_TEST_HOST_ROOT = dockerStateRoot;
+  };
+
+  system.activationScripts.systemdTsDockerState = {
+    text = ''
+      if [ -d /home/jem/systemd-ts ]; then
+        mkdir -p ${dockerStateRoot}
+        mkdir -p ${dockerStateRoot}/tests
+        chown jem:users ${dockerStateRoot}
+        chown jem:users ${dockerStateRoot}/tests
+        chmod 0777 ${dockerStateRoot}
+        chmod 0777 ${dockerStateRoot}/tests
+        ${pkgs.acl}/bin/setfacl -R -m u:501:rwx,u:1001:rwx ${dockerStateRoot} || true
+        ${pkgs.acl}/bin/setfacl -R -d -m u:501:rwx,u:1001:rwx ${dockerStateRoot} || true
+      fi
+    '';
+  };
+
+  systemd.tmpfiles.rules = [
+    "d ${dockerStateRoot} 0777 jem users - -"
   ];
 
   virtualisation.docker = {
